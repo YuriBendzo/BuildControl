dcaaccca5cfebc2d01e54ffc8cbb11d23c39bbce
xaim-dev <yurabendzo20@gmail.com>
2021-11-11 10:57:45 +0200

    added section why-us

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
-
-    $('.your-class').slick({
-    })
+    $('.why-us__slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
 });
